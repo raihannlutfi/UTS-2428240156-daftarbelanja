60a53d9847519f801ee7de78b809d66e5d31adb8
raihannlutfi <mraihanallutfi_2428240156@mhs.mdp.ac.id>
2026-04-14 11:59:53 +0700

    comit kedua

---
+++ UTS_Raihan/script.js
@@ -0,0 +1,57 @@
+let Nama = document.getElementById("nama");
+let jumlah = document.getElementById("jumlah");
+let keterangan = document.getElementById("keterangan");
+
+function simpan(){
+    console.log(nama_barang.value)
+    console.log(jumlah.value)
+    console.log(keterangan.value)
+    
+
+    // localStorage.setItem("npm",npm.value)
+    // localStorage.setItem("nama",nama.value)
+
+    // jika local storage ada isi atau value
+    if(localStorage.getItem("mahasiswa")===null){
+        // simpan array kososng []
+        localStorage.setItem("mahasiswa", "[]")
+    }
+    // panggil local storage (konversi string = > object)
+    let data = JSON.parse(localStorage.getItem("mahasiswa"))
+    console.log(data)
+
+    // simpan value npm dan nama ke dalam object data
+    data.push({
+        nama_barang: nama_barang.value,
+        jumlah: jumlah.value,
+        keterangan: keterangan.value
+    })
+    console.log(data)
+
+    // simpan data terbaru ke dalam local storage
+    // konversi dari object menjadi string
+    localStorage.setItem("mahasiswa", JSON.stringify(data))
+
+    // panggil tampil
+    tampil()
+}
+
+function tampil() {
+    // panggil local storage
+    let hasil = JSON.parse(localStorage.getItem("mahasiswa"))
+
+    // clear element ul id= list-mhs
+    document.getElementById("list-mhs").innerHTML=""
+
+    // lakukan perulangan ( forEach)
+    hasil.forEach(element => {
+        // console.log(element)
+        document.getElementById("list-mhs").innerHTML += `<div class="col-lg-4 col-md-6 col-sm-12">
+         <h4 class= "text-primary">${element.nama_barang}</h4> 
+         <h6 class="text-danger">${element.jumlah}</h6> 
+         <p>${element.keterangan}</p>
+        </div>`
+    });
+}
+// jalankan function tampil()
+tampil()
